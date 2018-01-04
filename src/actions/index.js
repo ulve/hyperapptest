d457eacb21c7fa23d81eb014ec88e81d6e1e7134
Olov Johansson <johansson.olov@gmail.com>
2018-01-04 13:48:45 +0100

    modifierat lite

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 export default {
   add: (/* event (e) */) => ({ num }) => ({ num: num + 1 }),
   sub: (/* event (e) */) => ({ num }) => ({ num: num - 1 }),
+  addHundred: (/* event (e) */) => ({num}) => ({num: num + 100})
 };
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,23 @@
-import { h } from 'hyperapp';
+import { h } from "hyperapp";
 
-/**
- * first object in the store is 'state' (an object - {})
- * second object in the store is 'actions' (an object - {})
- * here we destructure what is needed
- * 'num' from 'state' and 'add'/'sub' from 'actions'
- */
-export default ({ num }, { add, sub }) =>
+export default ({ num }, { add, sub, addHundred }) => (
   <div class="counter">
-    <h1>hyperapp-one</h1>
-    <p><em>With JSX and Webpack</em></p>
+    <h1>Hyperapp exempel</h1>
+    <p>
+      <em>JSX och Webpack</em>
+    </p>
     <hr />
     <section>
-      <button
-        class="sub"
-        onclick={sub}
-        disabled={num < 1}
-      >
+      <button class="sub" onclick={sub} disabled={num < 1}>
         -
       </button>
       <h1 class="count">{num}</h1>
-      <button
-        class="add"
-        onclick={add}
-      >
+      <button class="add" onclick={add}>
         +
       </button>
+      <button class ="add" onclick={addHundred}>
+      +100
+      </button>
     </section>
-  </div>;
+  </div>
+);
